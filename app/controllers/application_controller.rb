0065188beb1b3e65c6a10d437df787088ce113f1
Nirajan Pokharel <nirazan@nirazan.com.np>
2017-06-29 00:14:54 +0545

    exception notifier setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :prepare_exception_notifier
+
+  private
+  def prepare_exception_notifier
+    request.env["exception_notifier.exception_data"] = {
+      :current_user => current_user
+    }
+  end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :sparkpost
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :deliver_with => :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
+    :email_prefix => "[FeelHomeTravels Error]",
+    :sender_address => %{"notifier" <info@nirazan.com.np>},
+    :exception_recipients => %w{feelhome@nirazan.com.np}
+  }
 end
