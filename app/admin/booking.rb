e663928262a93d07d0beffb6b9629a377d452748
Nirajan Pokharel <nirazan@nirazan.com.np>
2017-07-01 00:03:56 +0545

    admin booking index updates

--- app/admin/booking.rb
+++ app/admin/booking.rb
@@ -11,5 +11,27 @@ ActiveAdmin.register Booking do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  index do
+    selectable_column
+    column :id
+    column :agent
+    column :user
+    column :flight_time
+    column :ticket_time_limit
+    column :flight_number
+    column :airline
+    column :origin
+    column :destination
+    column :pnr
+    column :ticket_number
+    column :bill_number
+    column :currency
+    column :amount
+    column :recipt_number
+    column :ticket_type
+    column :created_at
+    column :updated_at
+    actions :defaults => true
+  end
 
 end
